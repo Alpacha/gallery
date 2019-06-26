e93d39ee6923657d1826eeb5ed95ecbe9cef183d
Alpacha <42476207+Alpacha@users.noreply.github.com>
2019-06-26 12:10:25 +0300

    DOC: Added : gallery.json with massive with with data Edited : app.js added parse and added variable MODEL

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
-console.log(1);
+let model = JSON.pasre( $('#gallery.json').html());
+
+model = [
+
+    {
+        url: "./images/resized/homer.png",
+        alt: "Homer Simpson"
+    },
+    {
+        url: "./images/resized/bart.png",
+        alt: "Bart Simpson"
+    },
+    {
+        url: "./images/resized/marge.png",
+        alt: "Marge Simpson"
+    },
+    {
+        url: "./images/resized/liza.png",
+        alt: "Liza Simpson"
+    },
+    {
+        url: "./images/resized/grandpa.png",
+        alt: "Granpa Simpson"
+    },
+    {
+        url: "./images/resized/milhouse.png",
+        alt: "Milhouse"
+    },
+    {
+        url: "./images/resized/ralph.png",
+        alt: "Ralph Wigum"
+    },
+    {
+        url: "./images/resized/burns.png",
+        alt: "Mr Burns"
+    },
+    {
+        url: "./images/resized/edna.png",
+        alt: "Edna"
+    },
+    {
+        url: "./images/resized/chief.png",
+        alt: "Chief Wigum"
+    },
+    {
+        url: "./images/resized/ned.png",
+        alt: "Ned"
+    },
+    {
+        url: "./images/resized/krusty.png",
+        alt: "Krusty"
+    }
+];
