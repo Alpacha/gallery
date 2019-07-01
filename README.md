ea49f9bfe9542bd6173ab3bd213bb0998b23ea36
Alpacha <42476207+Alpacha@users.noreply.github.com>
2019-07-01 15:28:19 +0300

    DOC: edited README Feat : added cycle FOR to iterate an array ( 2 options )

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Simpsons Gallery
+Simple image gallery with features 
 ## Consists of :
 custom css ;
-css refresh to the default values ;
+css refresh to the default values ;  
 classic html ;
+data base
+## Steps
+1 - Creating HTML template
+2 - Making data base using gaalery.json
+3 - Loading app.js instead of html
+TBC
--- js/app.js
+++ js/app.js
@@ -1,53 +1,14 @@
-let model = JSON.pasre( $('#gallery.json').html());
+let model = require('../gallery.json');
+// console.log(model.model[7].alt)
 
-model = [
+// VARIANT ONE
+let arrayLength = model.length;
+for (let i = 0; i < arrayLength; i++) {
+    console.log(model.model[i]);
+}
 
-    {
-        url: "./images/resized/homer.png",
-        alt: "Homer Simpson"
-    },
-    {
-        url: "./images/resized/bart.png",
-        alt: "Bart Simpson"
-    },
-    {
-        url: "./images/resized/marge.png",
-        alt: "Marge Simpson"
-    },
-    {
-        url: "./images/resized/liza.png",
-        alt: "Liza Simpson"
-    },
-    {
-        url: "./images/resized/grandpa.png",
-        alt: "Granpa Simpson"
-    },
-    {
-        url: "./images/resized/milhouse.png",
-        alt: "Milhouse"
-    },
-    {
-        url: "./images/resized/ralph.png",
-        alt: "Ralph Wigum"
-    },
-    {
-        url: "./images/resized/burns.png",
-        alt: "Mr Burns"
-    },
-    {
-        url: "./images/resized/edna.png",
-        alt: "Edna"
-    },
-    {
-        url: "./images/resized/chief.png",
-        alt: "Chief Wigum"
-    },
-    {
-        url: "./images/resized/ned.png",
-        alt: "Ned"
-    },
-    {
-        url: "./images/resized/krusty.png",
-        alt: "Krusty"
-    }
-];
+// VARIANT TWO
+let index;
+for (index = 0; index < model.length; index++) {
+    console.log(model[index]);
+}
